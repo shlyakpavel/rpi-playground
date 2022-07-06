66770deaff1db04271bf390d0ba3fd30524edb28
Pavel <p.shlyak@servplus.ru>
2022-07-06 15:39:12 +0300

    No webengine

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Extract Qt
-tar -xf qt5-5.15.2-buster-pi3.tar.gz --exclude="qt5pi/include" --exclude="qt5pi/lib/libQt5WebEngineCore.so.5.15.2" \
-	--exclude="qt5pi/doc" --exclude="qt5pi/translations/qtwebengine_locales" --exclude="qt5pi/mkspecs"
+tar -xf qt5-5.15.2-buster-pi3.tar.gz --exclude="qt5pi/include" \
+	--exclude="qt5pi/doc" --exclude="qt5pi/mkspecs"
 tar -xf qtjsonserializer-5.15.2-buster*.tar.gz --strip-components 3
 tar -xf qtmqtt-5.15.2-buster-pi3*.tar.gz --strip-components 3
 # Install symlinks
