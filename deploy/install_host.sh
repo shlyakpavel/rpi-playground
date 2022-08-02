eeeafae8ac2a2c0aefc017546c6dae17d05c6748
Pavel <p.shlyak@servplus.ru>
2022-08-02 14:33:18 +0300

    Fix script

--- deploy/install_host.sh
+++ deploy/install_host.sh
@@ -1,3 +1,3 @@
-sudo tar -xf qtjsonserializer-5.15.2-buster-pi3-66770de.tar.gz --strip-components 3 -C /sysroot/usr/local/
-sudo tar -xf qtmqtt-5.15.2-buster-pi3-66770de.tar.gz --strip-components 3 -C /sysroot/usr/local/
-sudo tar -xf qt5-5.15.2-buster-pi3.tar.gz -C /sysroot/usr/local/
+sudo tar -xf qtjsonserializer-5.15.*-buster-pi3-*.tar.gz --strip-components 3 -C /sysroot/usr/local/
+sudo tar -xf qtmqtt-5.15.*-buster-pi3-*.tar.gz --strip-components 3 -C /sysroot/usr/local/
+sudo tar -xf qt5-5.15.*-buster-pi3.tar.gz -C /sysroot/usr/local/
