d316a5221af262dbc36af9e3d97bfda6eb9923e4
Шляк Павел Вячеславович <p.shlyak@servplus.ru>
2022-08-03 15:51:38 +0300

    Fix installer

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Extract Qt
-tar -xf qt5-5.15.*-buster-pi3.tar.gz --exclude="qt5pi/include" \
+tar -xf qt5-5.15.*-buster-*.tar.gz --exclude="qt5pi/include" \
 	--exclude="qt5pi/doc" --exclude="qt5pi/mkspecs"
 tar -xf qtjsonserializer-5.15.*-buster*.tar.gz --strip-components 3
-tar -xf qtmqtt-5.15.*-buster-pi3*.tar.gz --strip-components 3
+tar -xf qtmqtt-5.15.*-buster-*.tar.gz --strip-components 3
 # Install symlinks
 sudo ln -fs /home/pi/super/qt5pi /usr/local/
 echo '/usr/local/qt5pi/lib/' | sudo tee -a /etc/ld.so.conf.d/qt5pi.conf
